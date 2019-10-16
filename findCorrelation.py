e4e0bfd27db5b57337e2049b804e494f7c582741
xxdavis97 <43418361+xxdavis97@users.noreply.github.com>
2019-10-16 16:16:20 -0400

    Add files via upload

--- findCorrelation.py
+++ findCorrelation.py
@@ -15,6 +15,14 @@ usaServices = usaServices[usaServices['FREQUENCY'] == 'A']
 usaServices.rename(columns={'TIME': 'DATE'}, inplace=True)
 usaServices.set_index('DATE', inplace=True)
 
+goodsAndServices = pd.read_csv('Goods And Services Data (Long).csv')
+usaGoodsAndServices = goodsAndServices[(goodsAndServices['LOCATION'] == 'USA')]
+usaGoodsAndServices = usaGoodsAndServices[usaGoodsAndServices['FREQUENCY'] == 'A']
+usaGoodsAndServices.rename(columns={'TIME': 'DATE'}, inplace=True)
+usaGoodsAndServices.set_index('DATE', inplace=True)
+usaGoodsAndServicesEXP = usaGoodsAndServices[usaGoodsAndServices['SUBJECT'] == 'EXP']
+usaGoodsAndServicesEXP = usaGoodsAndServicesEXP[usaGoodsAndServicesEXP['MEASURE'] == 'MLN_USD']
+
 # EXPORTS
 usaGoodsEXP = usaGoods[usaGoods['SUBJECT'] == 'EXP']
 usaServicesEXP = usaServices[usaServices['SUBJECT'] == 'EXP']
@@ -39,6 +47,13 @@ expServicesDF['Inflation'] = flatInflation
 expServicesInflationCorr = expServicesDF.corr().values[0][1]
 print(expServicesInflationCorr)
 
+# Correlation between goods/services combined (-.627)
+expGoodsAndServicesDF = usaGoodsAndServicesEXP['Value'].to_frame()
+expGoodsAndServicesDF['Inflation'] = flatInflation[10:]
+expGoodsAndServicesInflationCorr = expGoodsAndServicesDF.corr().values[0][1]
+print(expGoodsAndServicesInflationCorr)
+
+
 impGoodsDF = usaGoodsIMP['Value'].to_frame()
 impGoodsDF['Inflation'] = flatInflation
 impGoodsInflationCorr = impGoodsDF.corr().values[0][1]
@@ -55,6 +70,20 @@ ax = plt.gca()
 ax2 = ax.twinx()
 ax.plot(usaGoodsEXP.index, usaGoodsEXP['Value'] / 1000, sns.xkcd_rgb["pale red"], label='US Goods')
 ax.plot(usaServicesEXP.index, usaServicesEXP['Value'] / 1000, sns.xkcd_rgb["amber"], label='US Services')
+ax2.plot(inflation.index, inflation['FPCPITOTLZGUSA'], label='Inflation')
+ax.set_title('US Goods and Services Exports Compared With Inflation')
+ax.set_xlabel('Year')
+ax.set_ylabel('US Exports (In Billions)')
+ax2.set_ylabel('Inflation Rate (%)')
+ax.legend()
+ax2.legend()
+# plt.show()
+
+# GOODS AND SERVICES
+plt.clf()
+ax = plt.gca()
+ax2 = ax.twinx()
+ax.plot(usaGoodsAndServicesEXP.index, usaGoodsAndServicesEXP['Value'] / 1000, sns.xkcd_rgb["pale red"], label='US Goods And Services')
 ax2.plot(inflation.index, inflation['FPCPITOTLZGUSA'], label='Inflation')
 ax.set_title('US Goods and Services Exports Compared With Inflation')
 ax.set_xlabel('Year')
--- gbrCorrelation.py
+++ gbrCorrelation.py
@@ -17,6 +17,14 @@ gbrServices.rename(columns={'TIME': 'DATE'}, inplace=True)
 gbrServices.set_index('DATE', inplace=True)
 gbrServices = gbrServices[3:]
 
+goodsAndServices = pd.read_csv('Goods And Services Data (Long).csv')
+gbrGoodsAndServices = goodsAndServices[(goodsAndServices['LOCATION'] == 'GBR')]
+gbrGoodsAndServices = gbrGoodsAndServices[gbrGoodsAndServices['FREQUENCY'] == 'A']
+gbrGoodsAndServices.rename(columns={'TIME': 'DATE'}, inplace=True)
+gbrGoodsAndServices.set_index('DATE', inplace=True)
+gbrGoodsAndServicesEXP = gbrGoodsAndServices[gbrGoodsAndServices['SUBJECT'] == 'EXP']
+gbrGoodsAndServicesEXP = gbrGoodsAndServicesEXP[gbrGoodsAndServicesEXP['MEASURE'] == 'MLN_USD']
+
 # EXPORTS
 gbrGoods = gbrGoods[gbrGoods['SUBJECT'] == 'EXP']
 gbrServices = gbrServices[gbrServices['SUBJECT'] == 'EXP']
@@ -37,6 +45,12 @@ ServicesDF = gbrServices['Value'].to_frame()
 ServicesDF['Inflation'] = flatInflation
 ServicesInflationCorr = ServicesDF.corr().values[0][1]
 print(ServicesInflationCorr)
+
+# Correlation between goods/services combined (-.655)
+expGoodsAndServicesDF = gbrGoodsAndServicesEXP['Value'].to_frame()
+expGoodsAndServicesDF['Inflation'] = flatInflation[10:]
+expGoodsAndServicesInflationCorr = expGoodsAndServicesDF.corr().values[0][1]
+print(expGoodsAndServicesInflationCorr)
 
 # PLOT ORTS
 fig = plt.figure()
