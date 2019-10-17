93e337ba7948ada62619fb1f2ceffa2c1f870aa1
xxdavis97 <43418361+xxdavis97@users.noreply.github.com>
2019-10-17 10:53:23 -0400

    Add files via upload

--- Project/runRegression.py
+++ Project/runRegression.py
@@ -5,6 +5,8 @@ import statsmodels.api as sm
 #SETUP DATA
 goodsAndServices = pd.read_csv('./Data/Goods And Services Data (Long).csv')
 gdp = pd.read_csv('./Data/GDP.csv')
+gdp.set_index('DATE', inplace=True)
+
 inflation = pd.read_csv('./Data/Inflation Data (Long).csv')
 inflation['DATE'] = pd.DatetimeIndex(inflation['DATE']).year
 inflation.set_index('DATE', inplace=True)
@@ -21,18 +23,18 @@ regressionFrame = pd.DataFrame()
 flatInflation = inflation.values.flatten()[10:]
 regressionFrame['Inflation'] = flatInflation
 regressionFrame.index = usaGoodsAndServicesEXP.index
-regressionFrame['Exports'] = usaGoodsAndServicesEXP['Value']
-regressionFrame['LaggedExports'] = regressionFrame['Exports'].shift(1)
-regressionFrame['GDP'] = gdp['GDP'].values.flatten()
-regressionFrame['LaggedGDP'] = regressionFrame['GDP'].shift(1)
-regressionFrame['LaggedInflation'] = regressionFrame['Inflation'].shift(1)
+regressionFrame['Change In Exports'] = usaGoodsAndServicesEXP[['Value']].pct_change()
+regressionFrame['Change In GDP'] = gdp.pct_change()['GDP'].values.flatten()
+# regressionFrame['LaggedGDP'] = regressionFrame['GDP'].shift(1)
+regressionFrame['Inflation Lag 1'] = regressionFrame['Inflation'].shift(1)
 regressionFrame.dropna(inplace=True)
 
 # PERFORM REGRESSION
-X = regressionFrame[['LaggedInflation', 'Exports', 'LaggedExports', 'GDP', 'LaggedGDP']]
+X = regressionFrame[['Inflation Lag 1', 'Change In Exports', 'Change In GDP']]
 y = regressionFrame['Inflation']
 X2 = sm.add_constant(X)
 est = sm.OLS(y, X2).fit()
+print(est.summary())
 
 
 
